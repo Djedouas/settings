f0a9241abfac933e533366c1590564600296c120
Jacky Volpes <jacky.volpes@oslandia.com>
2021-09-21 15:26:55 +0200

    Script d'installation

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s `readlink -f .bash_aliases` $HOME
+ln -s `readlink -f .mypy.ini` $HOME
+ln -s `readlink -f .pylintrc` $HOME
+ln -s `readlink -f .tigrc` $HOME
+ln -s `readlink -f .vimspector.json` $HOME
+ln -s `readlink -f .bash_completion` $HOME
+ln -s `readlink -f init.toml` $HOME/.SpaceVim.d/
+ln -s `readlink -f myspacevim.vim` $HOME/.SpaceVim.d/autoload/
+
