062632abdfa21f0d08dad9cb8c8ab7753e1abd15
Jacky Volpes <jacky.volpes@oslandia.com>
2025-02-13 14:47:17 +0100

    lutim: changement instance

--- flameshot_upload_lutim.sh
+++ flameshot_upload_lutim.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # lutim_instance="https://wtf.roflcopter.fr/pics/"
-lutim_instance="https://lutim.anartist.org/"
+lutim_instance="https://lutim.volpes.fr/"
 
 # Save image from clipboard to temp file
 rm /tmp/capture.png
 xclip -selection c -t image/png -o > /tmp/capture.png
+
+if [ ! -e /tmp/capture.png ] || [ $(file -bi /tmp/capture.png | sed -E "s/(.+);.*/\1/") != "image/png" ] || [ $(stat --printf=%s /tmp/capture.png) -lt 2 ]; then
+  notify-send -u critical "No image in clipboard"
+  exit -1
+fi
+
 
 # Upload image for 1 day to lutim service and read json response
 url=$(curl -F "format=json" -F "file=@/tmp/capture.png" -F "delete-day=1" $lutim_instance | \
