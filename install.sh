78bffbeaf0ccbef0f1d64d30e32f4f894bde22c0
Jacky Volpes <jacky.volpes@oslandia.com>
2025-02-13 14:23:49 +0100

    new config files (khard, khal, aerc)

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 rm $HOME/.config/lvim/config.lua
-ln -s `readlink -f config.lua` $HOME/.config/lvim
+ln -s `readlink -f lunarvim/config.lua` $HOME/.config/lvim
 
 rm $HOME/.config/lazygit/config.yml
-ln -s `readlink -f config.yml` $HOME/.config/lazygit
+ln -s `readlink -f lazygit/config.yml` $HOME/.config/lazygit
+
+rm $HOME/.config/khal/config
+ln -s `readlink -f khal/config` $HOME/.config/khal
+
+rm $HOME/.config/khard/khard.conf
+ln -s `readlink -f khard/khal.conf` $HOME/.config/khard
+
+ln -s `readlink -f aerc` $HOME/.config
 
 ln -s `readlink -f .bash_aliases` $HOME
 ln -s `readlink -f .mypy.ini` $HOME
@@ -13,4 +21,3 @@ ln -s `readlink -f .gdbinit` $HOME
 ln -s `readlink -f .vimspector.json` $HOME
 ln -s `readlink -f .bash_completion` $HOME
 ln -s `readlink -f qgisprinters` $HOME/.gdb/qgisprinters
-
