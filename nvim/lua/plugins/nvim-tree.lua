e38c48e1bd02c233ad8b97059a56338b962cd06b
Jacky Volpes <jacky.volpes@oslandia.com>
2025-07-29 18:25:20 +0200

    feat(nvim): ajout mappings pour nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,3 +1,11 @@
+local function my_on_attach(bufnr)
+  local api = require("nvim-tree.api")
+  vim.keymap.set("n", "l", api.node.open.edit,
+    { buffer = bufnr, noremap = true, silent = true, nowait = true })
+  vim.keymap.set("n", "h", api.node.navigate.parent_close,
+    { buffer = bufnr, noremap = true, silent = true, nowait = true })
+end
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -5,6 +13,8 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
-  keys = { { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" } }
+  opts = { on_attach = my_on_attach },
+  keys = {
+    { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
+  }
 }
