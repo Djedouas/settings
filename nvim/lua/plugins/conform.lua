2160916f75c4ad0b4087761614c43ae8d2465c91
Jacky Volpes <jacky.volpes@oslandia.com>
2025-07-30 18:40:13 +0200

    feat(nvim): add conform plugin for formatting

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,41 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>lf",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = { "n" },
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      python = { "isort", "black" },
+      cpp = { "clang-format", "astyle" },
+      lua = { "stylua" }
+    },
+    -- Set default options
+    default_format_opts = {
+      -- LSP formatting is used when no other formatters are available
+      lsp_format = "fallback",
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500 },
+    -- Customize formatters
+    formatters = {
+      astyle = {
+        prepend_args = { "--options", "/home/jacky/dev/QGIS/scripts/astyle.options" },
+      },
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
