4f4c9ce720a93956aa579a28e86ce591b687f8e9
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-01 16:35:36 +0200

    feat(nvim): mapping for moving lines

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -4,9 +4,11 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 
--- Windows width keymaps
+-- Windows resize keymaps
 vim.keymap.set("n", "<leader><", "<C-w>30<", { desc = "Lower window width" })
 vim.keymap.set("n", "<leader>>", "<C-w>30>", { desc = "Larger window width" })
+vim.keymap.set("n", "<C-Up>", "<cmd>:resize -2<CR>", { desc = "Lower window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>:resize +2<CR>", { desc = "Larger window height" })
 
 -- Buffer navigation
 vim.keymap.set("n", "<leader>bn", "<cmd>:bnext<CR>", { desc = "Next buffer" })
@@ -18,3 +20,8 @@ vim.keymap.set("n", "<leader><tab>", "<C-^>", { desc = "Go to last buffer" })
 vim.keymap.set("n", "<leader>h", "<cmd>:nohlsearch<CR>", { desc = "No highlight" })
 vim.keymap.set("n", "<leader>w", "<cmd>:w<CR>", { desc = "Save buffer" })
 vim.keymap.set({ "n", "o", "x" }, "²", "<cmd>set wrap!<CR>", { desc = "Toggle word wrap" })
+
+vim.keymap.set("x", "<M-j>", ":move '>+1<CR>gv-gv", { desc = "Move lines down" })
+vim.keymap.set("n", "<M-j>", ":move .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("x", "<M-k>", ":move '<-2<CR>gv-gv", { desc = "Move lines up" })
+vim.keymap.set("n", "<M-k>", ":move .-2<CR>==", { desc = "Move line up" })
