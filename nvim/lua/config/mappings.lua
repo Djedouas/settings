9928b051ec4c8d83e39b7cb7acb1543d550cd272
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-01 12:52:20 +0200

    chore(nvim): reorder mappings

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,22 +1,20 @@
--- Move cursor windows
+-- Move between windows
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 
-vim.keymap.set("n", "<leader><tab>", "<C-^>", { desc = "Go to last buffer" })
-
 -- Windows width keymaps
 vim.keymap.set("n", "<leader><", "<C-w>30<", { desc = "Lower window width" })
 vim.keymap.set("n", "<leader>>", "<C-w>30>", { desc = "Larger window width" })
-
--- Toggle wrap
-vim.keymap.set({ "n", "o", "x" }, "²", "<cmd>set wrap!<CR>", { desc = "Toggle word wrap" })
 
 -- Buffer navigation
 vim.keymap.set("n", "<leader>bn", "<cmd>:bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bb", "<cmd>:bprevious<CR>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>c", "<cmd>:bdelete<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader><tab>", "<C-^>", { desc = "Go to last buffer" })
 
 -- Others
 vim.keymap.set("n", "<leader>h", "<cmd>:nohlsearch<CR>", { desc = "No highlight" })
+vim.keymap.set("n", "<leader>w", "<cmd>:w<CR>", { desc = "Save buffer" })
+vim.keymap.set({ "n", "o", "x" }, "²", "<cmd>set wrap!<CR>", { desc = "Toggle word wrap" })
