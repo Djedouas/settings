8b64b2d76ec5d78368021fb3fb5408f183512b80
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-01 13:10:13 +0200

    feat(nvim): configure lua_ls pour neovim

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,2 +1,25 @@
 require("lspconfig").clangd.setup({})
-require("lspconfig").lua_ls.setup({})
+
+require("lspconfig").lua_ls.setup({
+  on_init = function(client)
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        version = "LuaJIT",
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
