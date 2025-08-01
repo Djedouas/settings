7871ca39bd06814aff6743bfaf73cc106b44e3c5
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-01 16:39:03 +0200

    chore(nvim): formatting

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,16 +3,15 @@ local function telescope()
 end
 
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { "<leader>ff", telescope().find_files, desc = 'Find file' },
-    { "<leader>bf", telescope().buffers, desc = 'Find buffer' },
-    { "<leader>sr", telescope().oldfiles, desc = 'Find recent file' },
-    { "<leader>sR", telescope().registers, desc = 'Show registers' },
-    { "<leader>sk", telescope().keymaps, desc = 'Show keymaps' },
-    { "<leader>sl", telescope().resume, desc = 'Resume last search' },
-    { "<leader>sc", telescope().colorscheme, desc = 'Choose colorscheme' },
-  }
+    { "<leader>ff", telescope().find_files, desc = "Find file" },
+    { "<leader>bf", telescope().buffers, desc = "Find buffer" },
+    { "<leader>sr", telescope().oldfiles, desc = "Find recent file" },
+    { "<leader>sk", telescope().keymaps, desc = "Show keymaps" },
+    { "<leader>sl", telescope().resume, desc = "Resume last search" },
+    { "<leader>sc", telescope().colorscheme, desc = "Choose colorscheme" },
+  },
 }
