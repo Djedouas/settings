79b23e08e3c26c5dcad686c9a0b2dba131fe634b
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-01 12:51:27 +0200

    feat(nvim): add gitsigns mappings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,11 @@
 return {
-  "lewis6991/gitsigns.nvim"
+  "lewis6991/gitsigns.nvim",
+  lazy=false,
+  keys = {
+    { "<leader>gj", "<cmd>:Gitsigns next_hunk<CR>", desc = "Next git hunk" },
+    { "<leader>gk", "<cmd>:Gitsigns prev_hunk<CR>", desc = "Previous git hunk" },
+    { "<leader>gp", "<cmd>:Gitsigns preview_hunk<CR>", desc = "Preview git hunk" },
+    { "<leader>gr", "<cmd>:Gitsigns reset_hunk<CR>", desc = "Reset git hunk" },
+    { "<leader>gl", "<cmd>:Gitsigns blame<CR>", desc = "Git blame" },
+  },
 }
