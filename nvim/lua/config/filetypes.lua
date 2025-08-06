e2872cf56e35cfbcb875234aa15ea7a72a8daa1b
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-06 16:39:06 +0200

    feat(nvim): mapping recent files on startup

--- nvim/lua/config/filetypes.lua
+++ nvim/lua/config/filetypes.lua
@@ -8,3 +8,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt["expandtab"] = true
   end,
 })
+
+-- Start screen
+if vim.fn.argc() == 0 then
+  vim.api.nvim_create_autocmd("BufEnter", {
+    once = true,
+    callback = function() vim.keymap.set("n", "r", require("telescope.builtin").oldfiles, { buffer = true, silent = true }) end,
+  })
+end
