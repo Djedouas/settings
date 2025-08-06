1b9abc702f96b4af8e5902ffe7009c4123873b6e
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-06 17:33:49 +0200

    chore(nvim): minor formatting

--- nvim/lua/config/filetypes.lua
+++ nvim/lua/config/filetypes.lua
@@ -3,9 +3,9 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   callback = function()
-    vim.opt["softtabstop"] = 2
-    vim.opt["shiftwidth"] = 2
-    vim.opt["expandtab"] = true
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.expandtab = true
   end,
 })
 
