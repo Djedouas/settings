bd89aba6a1aca3039662001040d8296cc0b7f454
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-06 18:53:59 +0200

    feat(nvim): python venv lualine component

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,24 @@
+local function get_venv()
+  local venv = os.getenv("VIRTUAL_ENV")
+  if venv ~= nil and string.find(venv, "/") then
+    local orig_venv = venv
+    for w in orig_venv:gmatch("([^/]+)") do
+      venv = w
+    end
+    venv = string.format("(%s)", venv)
+  else
+    venv = "NO VENV"
+  end
+  return venv
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
+    sections = {
+      lualine_y = { { get_venv, cond = function() return vim.bo.filetype == "python" end } },
+    },
     extensions = { "nvim-tree", "symbols-outline" },
   },
 }
