e2c9a13d671fcc895323540af744b3aeede8c75d
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-07 12:37:55 +0200

    feat(nvim): add ltex_plus lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,7 +3,9 @@ vim.lsp.enable("ruff")
 vim.lsp.enable("pyright")
 vim.lsp.enable("clangd")
 vim.lsp.enable("lua_ls")
+vim.lsp.enable("ltex_plus")
 
+-- Configuration for lua_ls
 vim.lsp.config("lua_ls", {
   on_init = function(client)
     client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
@@ -25,6 +27,28 @@ vim.lsp.config("lua_ls", {
     Lua = {},
   },
 })
+
+-- Configuration for ltex_plus
+local function toggle_language()
+  local ltex_settings = vim.lsp.get_clients({ name = "ltex_plus" })[1].config.settings
+  if not ltex_settings then return end
+  local language = ltex_settings.ltex.language
+  if language == "fr" then
+    ltex_settings.ltex.language = "en-US"
+  else
+    ltex_settings.ltex.language = "fr"
+  end
+  vim.lsp.buf_notify(0, "workspace/didChangeConfiguration", { settings = ltex_settings })
+end
+vim.keymap.set("n", "<leader>lm", toggle_language, { desc = "Toggle language" })
+
+-- Toggle inlay hints
+vim.keymap.set(
+  "n",
+  "<leader>lh",
+  function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
+  { desc = "Toggle inlay hints" }
+)
 
 -- nvim-lspconfig plugin
 return {
