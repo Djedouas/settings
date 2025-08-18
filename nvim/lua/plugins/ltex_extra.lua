d2fb9df3067a7e1fbeea8fd70a9c23880dcca8f5
Jacky Volpes <jacky.volpes@oslandia.com>
2025-08-18 15:57:04 +0200

    feat(nvim-ltex-extra): add ltex_extra for handling of dictionnaries and code actions

---
+++ nvim/lua/plugins/ltex_extra.lua
@@ -0,0 +1,7 @@
+return {
+  "barreiroleo/ltex_extra.nvim",
+  branch = "dev", -- master branch is considered deprecated, see https://github.com/barreiroleo/ltex_extra.nvim/issues/70#issuecomment-2993815422
+  opts = {
+    path = ".local/share/ltex",
+  },
+}
