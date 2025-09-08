888e9f6566cfca6883c38dd87072909f42b9648f
Jacky Volpes <jacky.volpes@oslandia.com>
2025-09-08 17:59:19 +0200

    fix(nvim): remove unnecessary parens

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,13 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master", -- will be main in the future
-    lazy = false, -- nvim-treesitter does not support lazy loading
-    build = ":TSUpdate", -- when nvim-treesitter is updated, we need to update its parsers
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master", -- will be main in the future
+  lazy = false, -- nvim-treesitter does not support lazy loading
+  build = ":TSUpdate", -- when nvim-treesitter is updated, we need to update its parsers
 
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "cpp", "python", "lua" },
-        highlight = { enable = true },
-      })
-    end,
-  },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "cpp", "python", "lua" },
+      highlight = { enable = true },
+    })
+  end,
 }
