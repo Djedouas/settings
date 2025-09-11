23711ffecf6b4249adcbbde0d0d79d23f6434e3d
Jacky Volpes <jacky.volpes@oslandia.com>
2025-09-11 11:28:12 +0200

    chore(nvim): update zeal docset preset to qt6

--- nvim/lua/plugins/zeavim.lua
+++ nvim/lua/plugins/zeavim.lua
@@ -1,5 +1,5 @@
-vim.api.nvim_create_autocmd("BufEnter", { pattern = { "*.py" }, command = "Docset qgis,qt5,python" })
-vim.api.nvim_create_autocmd("BufEnter", { pattern = { "*.cpp", "*.h" }, command = "Docset cpp,qgis,qt5" })
+vim.api.nvim_create_autocmd("BufEnter", { pattern = { "*.py" }, command = "Docset qgis,qt6,python" })
+vim.api.nvim_create_autocmd("BufEnter", { pattern = { "*.cpp", "*.h" }, command = "Docset cpp,qgis,qt6" })
 
 return {
   "KabbAmine/zeavim.vim",
