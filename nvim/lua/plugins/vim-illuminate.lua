5487f32250d7ffffade214c4d2ec728b56e7ba5d
Jacky Volpes <jacky.volpes@oslandia.com>
2025-10-13 14:43:34 +0200

    fix(nvim-illuminate): ignore nvim-tree

--- nvim/lua/plugins/vim-illuminate.lua
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -5,6 +5,9 @@ return {
     require("illuminate").configure({
       delay = 1000,
       min_count_to_highlight = 2,
+      filetypes_denylist = {
+        "NvimTree",
+      },
     })
   end,
 }
