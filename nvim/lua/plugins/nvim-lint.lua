f0aa07042119da3bc356fd556d1add35ed5d1bef
Jacky Volpes <jacky.volpes@oslandia.com>
2026-01-05 14:27:58 +0100

    feat(nvim-lint): manage python linters

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,20 +1,17 @@
 -- Linters by filetype, with optional command if name differs
 local linters = {
-  python = {
-    { name = "flake8" },
-    { name = "pylint" },
-    { name = "mypy" },
-  },
-  cpp = {
-    -- { name = "clangtidy", cmd = "clang-tidy" },
-    -- { name = "clazy" },
-  },
+  python = { "flake8", "pylint", "mypy" },
 }
 
 return {
   "mfussenegger/nvim-lint",
   lazy = false,
   config = function()
+    -- Configure pylint to add pylintrc if it exists
+    local rcfile_path = vim.fn.getcwd() .. "/linters/pylintrc"
+    local pylint = require("lint").linters.pylint
+    if vim.loop.fs_stat(rcfile_path) then table.insert(pylint.args, "--rcfile=" .. rcfile_path) end
+
     -- Lint files on write when the specified linters are found
     vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
       pattern = { "*.py", "*.cpp" },
@@ -23,12 +20,11 @@ return {
         local ft_linters = linters[vim.bo.filetype]
         if not ft_linters then return end
         for _, linter in ipairs(ft_linters) do
-          -- Get executable name (cmd prevent on name)
-          local cmd = linter.cmd or linter.name
-
           -- If it is executable, lint at the end of the current event loop
           -- (so that the current working directory is correctly set by project.nvim)
-          if vim.fn.executable(cmd) == 1 then vim.schedule(function() require("lint").try_lint(linter.name) end) end
+          if vim.fn.executable(require("lint").linters[linter].cmd) == 1 then
+            vim.schedule(function() require("lint").try_lint(linter) end)
+          end
         end
       end,
     })
