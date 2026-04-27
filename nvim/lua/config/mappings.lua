f5ce189af88a59d47c902c87c6c92822db307499
Jacky Volpes <jacky.volpes@oslandia.com>
2026-04-27 11:42:05 +0200

    chore(nvim): move mapping to a better place

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -67,6 +67,37 @@ vim.keymap.set(
   { desc = "Check file existance" }
 )
 
+-- Temporary mapping for  start screen only
+if vim.fn.argc() == 0 then
+  vim.api.nvim_create_autocmd("BufEnter", {
+    once = true,
+    callback = function()
+      local buf = vim.api.nvim_get_current_buf()
+
+      -- Only apply this logic if the buffer is completely empty and unnamed
+      if vim.api.nvim_buf_get_name(buf) == "" and vim.api.nvim_buf_line_count(buf) == 1 then
+        -- Create a temporary keymap: "r" opens Telescope oldfiles
+        vim.keymap.set("n", "r", require("telescope.builtin").oldfiles, { buffer = buf, silent = true })
+
+        -- Remove the keymap as soon as the user starts typing something
+        vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+          buffer = buf,
+          once = true,
+          callback = function()
+            -- Check if the buffer actually has some content
+            local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+            local has_content = #lines > 1 or (lines[1] ~= "")
+            if has_content then
+              -- Safely delete the keymap without errors
+              pcall(vim.keymap.del, "n", "r", { buffer = buf })
+            end
+          end,
+        })
+      end
+    end,
+  })
+end
+
 -- Others
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "No highlight", silent = true })
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save buffer", silent = true })
